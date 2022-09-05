aa655542debb0b3aa430f09d5d3a8b432eeac991
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2022-09-05 18:43:57 +0200

    Add LINQ extension examples

---
+++ Pre.Linq.Extensions.Cons/LinqExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pre.Linq.Extensions.Cons
+{
+    public static class LinqExtensions
+    {
+        private static readonly Random random = new Random();
+
+        public static IEnumerable<int> ElementsAtEvenPositions(this IEnumerable<int> enumerable)
+        {
+            bool evenIndex = true;
+            foreach(int number in enumerable)
+            {
+                if (evenIndex)
+                {
+                    yield return number;
+                }
+                evenIndex = !evenIndex;
+            }
+        }
+
+        public static IEnumerable<T> ElementsAtEvenPositions<T>(this IEnumerable<T> enumerable)
+        {
+            bool evenIndex = true;
+            foreach (T element in enumerable)
+            {
+                if (evenIndex)
+                {
+                    yield return element;
+                }
+                evenIndex = !evenIndex;
+            }
+        }
+
+        public static int TakeRandom(this IEnumerable<int> enumerable)
+        {
+            int randomIndex = random.Next(enumerable.Count());
+            return enumerable.ElementAt(randomIndex);
+        }
+
+        public static T TakeRandom<T>(this IEnumerable<T> enumerable)
+        {
+            int randomIndex = random.Next(enumerable.Count());
+            return enumerable.ElementAt(randomIndex);
+        }
+    }
+}
--- Pre.Linq.Extensions.Cons/Program.cs
+++ Pre.Linq.Extensions.Cons/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Pre.Linq.Extensions.Cons;
+
+List<int> numbers = new() { 1, 3, 0, 6, 8, 11, 14, 8, 2 };
+List<string> names = new() { "Alice", "Bob", "Carol", "Dave", "Eve" };
+
+var numbersAtEvenPosition = numbers.ElementsAtEvenPositions();
+
+Console.WriteLine("Numbers at even positions:");
+Console.WriteLine("--------------------------");
+foreach(var number in numbersAtEvenPosition)
+{
+    Console.WriteLine(number);
+}
+Console.WriteLine();
+
+var namesAtEvenPosition = names.ElementsAtEvenPositions();
+Console.WriteLine("Names at even positions:");
+Console.WriteLine("------------------------");
+foreach(var name in namesAtEvenPosition)
+{
+    Console.WriteLine(name);
+}
+Console.WriteLine();
+
+var randomNameAtEvenPosition = names.ElementsAtEvenPositions().TakeRandom();
+Console.WriteLine("Random name at even position:");
+Console.WriteLine("-----------------------------");
+Console.WriteLine($"Result: {randomNameAtEvenPosition}");
+Console.WriteLine();
+
+var sumNumbersAtEvenPositionSkipFirstTwo = numbers
+    .Skip(2)
+    .ElementsAtEvenPositions()
+    .Sum();
+Console.WriteLine("Sum of numbers at even position (skip first two):");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine($"Result: {sumNumbersAtEvenPositionSkipFirstTwo}");
+
